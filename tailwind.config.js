a28a24f8d664e9b4d1d92a8d4de9d36ad386e35a
Alexandre Parent <alexp90300@gmail.com>
2025-08-06 11:19:43 +0200

    création de bloc linéaire desktop avec variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,19 +17,20 @@ module.exports = {
         'h1-desktop': ['140px'],
         'h1-tablet': ['120px'],
         'h1-mobile': ['44px'],
+        'h2-large': ['50px', { lineHeight: '60px' }],
         'subtitle-desktop': ['26px', { lineHeight: '1.15' }],
         'subtitle-mobile': ['22px', { lineHeight: '1.15' }],
-        'tag': ['16px', { lineHeight: '1.25' }],
+        'tag': ['16px', { lineHeight: '20px' }],
       },
       colors: {
         'pure-white': '#FFFFFF',
         'vibrant-blue': '#451DC7',
         'black': '#000000',
-        'energy-green': '#17B285',
+        'energy-green': '#04F06A',
         'dark-purple': '#4325C4',
       },
       backgroundImage: {
-        'radial-cards': 'radial-gradient(ellipse at center, rgba(67, 37, 196, 0.1) 0%, rgba(23, 178, 133, 0.1) 100%)',
+        'radial-cards': 'radial-gradient(104.71% 432.93% at 29.43% 37.21%, #451DC7 0%, #250F6B 100%)',
         'radial-page': 'linear-gradient(68deg,rgba(67, 37, 196, 1) 0%, rgba(23, 178, 133, 1) 100%)',
       },
       spacing: {
